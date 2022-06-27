b13a5719232e4c2fda4f86c5c96546bd6de3691a
tntnam <17520785@ms.uit.edu.vn>
2022-06-28 02:09:59 +0700

    Update PiranhaPlant

--- Mario-game/PiranhaPlant.cpp
+++ Mario-game/PiranhaPlant.cpp
@@ -1,1 +1,88 @@
 #include "PiranhaPlant.h"
+
+CPiranhaPlant::CPiranhaPlant(float x, float y)
+{
+	this->x_start = x;
+	this->y_start = y;
+	this->lastAni = -1;
+
+	isMovingDown = false;
+	isIdlingUp = false;
+	isIdlingDown = false;
+
+
+	start_idlingUp = 0;
+	start_idlingDown = 0;
+
+	this->SetState(PIRANHA_PLANT_STATE_GROWING_UP);
+}
+
+void CPiranhaPlant::GetBoundingBox(float& left, float& top, float& right, float& bottom)
+{
+	left = x - PIRANHA_PLANT_BBOX_WIDTH / 2;
+	top = y - PIRANHA_PLANT_BBOX_HEIGHT / 2;
+	right = left + PIRANHA_PLANT_BBOX_WIDTH;
+	bottom = top + PIRANHA_PLANT_BBOX_HEIGHT;
+}
+
+void CPiranhaPlant::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
+{
+	if ((this->y <= y_start - PIRANHA_PLANT_BBOX_HEIGHT) && isGrowingUp)
+	{
+		StopGrowingUp();
+		SetState(PIRANHA_PLANT_STATE_IDLING_UP);
+	}
+	if ((GetTickCount64() - start_idlingUp > PIRANHA_PLANT_IDLING_UP_TIMEOUT) && isIdlingUp)
+	{
+		StopIdlingUp();
+		SetState(PIRANHA_PLANT_STATE_MOVING_DOWN);
+	}
+	if ((this->y >= y_start) && isMovingDown)
+	{
+		StopMovingDown();
+		SetState(PIRANHA_PLANT_STATE_IDLING_DOWN);
+	}
+	if ((GetTickCount64() - start_idlingDown > PIRANHA_PLANT_IDLING_DOWN_TIMEOUT) && isIdlingDown)
+	{
+		StopIdlingDown();
+		SetState(PIRANHA_PLANT_STATE_GROWING_UP);
+	}
+	CGameObject::Update(dt, coObjects);
+	CCollision::GetInstance()->Process(this, dt, coObjects);
+}
+
+void CPiranhaPlant::Render()
+{
+	int aniId = ID_ANI_PIRANHA_PLANT;
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CPiranhaPlant::OnNoCollision(DWORD dt)
+{
+	y += vy * dt;
+}
+
+void CPiranhaPlant::SetState(int state)
+{
+	switch (state)
+	{
+	case PIRANHA_PLANT_STATE_GROWING_UP:
+		vy = -PIRANHA_PLANT_SPEED;
+		StartGrowingUp();
+		break;
+	case PIRANHA_PLANT_STATE_MOVING_DOWN:
+		vy = PIRANHA_PLANT_SPEED;
+		StartMovingDown();
+		break;
+	case PIRANHA_PLANT_STATE_IDLING_UP:
+		vy = 0;
+		StartIdlingUp();
+		break;
+	case PIRANHA_PLANT_STATE_IDLING_DOWN:
+		vy = 0;
+		StartIdlingDown();
+		break;
+	}
+	CGameObject::SetState(state);
+}
--- Mario-game/PiranhaPlant.h
+++ Mario-game/PiranhaPlant.h
@@ -2,24 +2,36 @@
 
 #include "GameObject.h"
 
-#define PIRANHAPLANT_SPEED 0.015f
-#define VENUS_FIRE_TRAP_SPEED 0.015f
+#define PIRANHA_PLANT_SPEED 0.035f
 
-#define PIRANHAPLANT_GREEN_BBOX_WIDTH 16
-#define PIRANHAPLANT_BBOX_HEIGHT 24
+#define PIRANHA_PLANT_BBOX_WIDTH 16
+#define PIRANHA_PLANT_BBOX_HEIGHT 24
+
+#define PIRANHA_PLANT_STATE_GROWING_UP 100
+#define PIRANHA_PLANT_STATE_MOVING_DOWN 200
+#define PIRANHA_PLANT_STATE_IDLING_UP 300
+#define PIRANHA_PLANT_STATE_IDLING_DOWN 400
 
 
+#define PIRANHA_PLANT_IDLING_UP_TIMEOUT			1000
+#define PIRANHA_PLANT_IDLING_DOWN_TIMEOUT		750
 
-#define PIRANHA_PLANT 1
-
-
-#define PIRANHAPLANT_TIME	1000
+#define ID_ANI_PIRANHA_PLANT	8000
 
 class CPiranhaPlant : public CGameObject
 {
 protected:
-	float marioXX, marioYY;
-	float oldX, oldY;
+	float x_start, y_start;
+	int lastAni;
+
+	BOOLEAN isGrowingUp;
+	BOOLEAN isMovingDown;
+	BOOLEAN isIdlingUp;
+	BOOLEAN isIdlingDown;
+
+	ULONGLONG start_idlingUp;
+	ULONGLONG start_idlingDown;
+
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
@@ -27,10 +39,50 @@ protected:
 	virtual int IsBlocking() { return 0; }
 	virtual void OnNoCollision(DWORD dt);
 
-	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e) {};
 
 public:
 	CPiranhaPlant(float x, float y);
 	virtual void SetState(int state);
-	void Appear();
+
+
+	//start
+	void StartGrowingUp()
+	{
+		isGrowingUp = true;
+	}
+	void StartMovingDown()
+	{
+		isMovingDown = true;
+	}
+	void StartIdlingUp()
+	{
+		isIdlingUp = true;
+		start_idlingUp = GetTickCount64();
+	}
+	void StartIdlingDown()
+	{
+		isIdlingDown = true;
+		start_idlingDown = GetTickCount64();
+	}
+
+	//stop
+	void StopGrowingUp()
+	{
+		isGrowingUp = false;
+	}
+	void StopMovingDown()
+	{
+		isMovingDown = false;
+	}
+	void StopIdlingUp()
+	{
+		isIdlingUp = false;
+		start_idlingUp = 0;
+	}
+	void StopIdlingDown()
+	{
+		isIdlingDown = false;
+		start_idlingDown = 0;
+	}
 };
